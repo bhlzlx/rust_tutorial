45818d21e729ac146a611bc6258f23d2d59e530a
新 李 <bhlzlx@hotmail.com>
2021-03-11 22:49:09 +0800

    更新Heap

--- gx/src/excel/core/heap.rs
+++ gx/src/excel/core/heap.rs
@@ -1,21 +1,47 @@
 use std::alloc::*;
 
-struct HeapPage<cap:u32> {
-    let ptr: *mut u8,   // 内存指针
-    let position: u32;  // 分配位置
+struct HeapPage<'a> {
+    ptr: *mut u8,   // 内存指针
+    position: u32,  // 分配位置
+    capacity: u32,
+    full_slice:&'a mut [u8],
 }
 
-impl HeapPage<cap:u32> {
-    fn new()->HeapPage {
+impl HeapPage<'_> {
+    fn new(capacity: u32)->HeapPage<'static> {
         let mut ptr = std::ptr::null_mut();
         unsafe {
-            let memlayout = Layout::from_size_align_unchecked(cap, 16);
+            let memlayout = Layout::from_size_align_unchecked(capacity as usize, 16);
             ptr = alloc(memlayout);
+        
+            let page: HeapPage = HeapPage {
+                ptr: ptr,
+                position: 0,
+                capacity: capacity,
+                full_slice: std::slice::from_raw_parts_mut(ptr, capacity as usize)
+            };
+            return page;
         }
-        let page: HeapPage = HeapPage {
-            ptr: ptr,
-            position: 0
+    }
+
+    fn alloc(&mut self, size: u32)->Option<&mut[u8]> {
+        if (self.capacity - self.position) >= size {
+            let begin = self.position as usize;
+            let end = begin + size as usize;
+            let rst = &mut self.full_slice[begin..end];
+            self.position += size;
+            return Some(rst);
+        } else {
+            return None;
         }
-        return page;
+    }
+}
+
+impl Drop for HeapPage<'_> {
+    fn drop(&mut self) {
+        unsafe {
+            let layout = Layout::from_size_align_unchecked(self.capacity as usize, 16);
+            std::alloc::dealloc(self.ptr, layout);
+        }
     }
 }
--- gx/src/lib.rs
+++ gx/src/lib.rs
@@ -1,2 +1,1 @@
 pub mod excel;
-use self::excel::core;
