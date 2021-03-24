521ce3969bdc81fd5d656ec0755cd37812dde648
新 李 <bhlzlx@hotmail.com>
2021-03-25 00:09:10 +0800

    update

--- gx/src/excel/core.rs
+++ gx/src/excel/core.rs
@@ -1,5 +1,6 @@
 pub mod heap;
 pub mod types;
+pub mod sparse_storage;
 
 pub fn hello_core() {
     println!("hello,core!")
--- gx/src/excel/core/sparse_storage.rs
+++ gx/src/excel/core/sparse_storage.rs
@@ -1,9 +1,34 @@
-use heap;
+struct MemoryChunk {
+    prev:*const MemoryChunk,
+    capacity: usize,
+    position: usize,
+    buffer:[u8;0]
+}
 
-struct Column {
+impl MemoryChunk {
+    fn new(capacity: usize, heap:*mut super::heap::Heap)->*mut MemoryChunk {
+        unsafe {
+            let heap_ref = &mut *heap;
+            let size_need = std::mem::size_of::<MemoryChunk>() + capacity;
+            let mem_ptr:*mut u8 = heap_ref.allocate(size_need);
+            let rst:*mut MemoryChunk = mem_ptr as *mut MemoryChunk;
+            let rst_ref = &mut *rst;
+            rst_ref.position = 0;
+            rst_ref.capacity = capacity;
+            return rst;
+        }
+    }
+}
 
+struct ColumnStorage {
+    chunk: *const MemoryChunk,
+}
+
+impl ColumnStorage {
+    pub fn write<T>(value: T) {
+    }
 }
 
 pub struct SparseStorage {
-    
+
 }
